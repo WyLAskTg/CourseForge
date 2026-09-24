238f9449e2493edad28ad86b31d570d2a68ebb86
RY Wang <shanxiwangruoyi@163.com>
2026-09-24 00:31:31 -0400

    Remove obsolete docs and correct announcement

--- src/announcements.js
+++ src/announcements.js
@@ -4,6 +4,6 @@ export const PRODUCT_ANNOUNCEMENTS = [
     id: "update-2026-09-24-overview",
     date: "2026-09-24",
     title: "常规更新 / Regular Update",
-    body: "优化了收藏夹功能。 / Optimized the page layout."
+    body: "优化了页面布局。 / Optimized the page layout."
   },
 ];
